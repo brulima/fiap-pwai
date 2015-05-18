3788dd41739ba441af2c3a77fd9bab6a3b603e19
Bruna Lima <bru.allima@gmail.com>
2015-05-18 20:07:26 -0300

    Criando projetos endereco-ws e usuario-web

---
+++ endereco-ws/src/com/fiap/endereco/to/EnderecoTO.java
@@ -0,0 +1,13 @@
+package com.fiap.endereco.to;
+
+import java.io.Serializable;
+
+public class EnderecoTO implements Serializable{
+	private String cep;
+	private String logradouro;
+	private String bairro;
+	private String cidade;
+	private String estado;
+	private String complemento;
+	private int numero;
+}
